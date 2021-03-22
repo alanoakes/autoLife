d63d36f87715d27c6973238a8abde7af2878b895
Parker Oakes <parker.oakes@clkmm.com>
2021-03-22 10:29:01 -0500

    initial commit

---
+++ lifeAuto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# File Description: Life Automation
+
+Blessings=( God Family Church Work Friends  )
+Life=( Joy Peace Obstacles Pain Fear Regret  )
+
+for i in ${Life[@]}; do
+	for j in ${Blessings[@]}; do
+
+		echo "Try hard to figure out $i ... trying ... still trying!!"
+		echo "$i has not been figured out! Now learning to enjoy $j ;)"
+
+	done
+done
